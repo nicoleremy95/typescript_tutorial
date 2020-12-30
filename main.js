cf6948c244a9eaefdeda51186682c77a7020bdf6
Nicole Remy <nicoleremy95@gmail.com>
2020-12-30 11:26:52 -0800

    Declarations code and notes

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 "use strict";
 exports.__esModule = true;
-var message = 'Hello World';
+var message = 'Hello ';
 console.log(message);
+var x = 10;
+var y = 20;
+//let declarations do not need to be initialized, whereas const does
+var sum;
+var title = 'Codevolution';
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 export{};
-let message = 'Hello World';
+let message = 'Hello ';
 console.log(message);
+
+let x = 10;
+const y = 20;
+
+//let declarations do not need to be initialized, whereas const does
+let sum; 
+const title = 'Codevolution'
